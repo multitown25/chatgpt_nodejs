144ae4141c68b32606ee8f8a15c4797d3fe6c90e
Ramazan <multitown25@gmail.com>
2025-01-29 15:10:08 +0300

    Updates

--- src/app.js
+++ src/app.js
@@ -13,14 +13,14 @@ const app = express();
 app.use(express.json());
 
 // Маршруты
-app.use('/payment', paymentRoutes);
-app.use('/webhook', webhookRoutes);
-
-// Обработка ошибок
-app.use((err, req, res, next) => {
-    console.error('Unhandled error:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-});
+// app.use('/payment', paymentRoutes);
+// app.use('/webhook', webhookRoutes);
+//
+// // Обработка ошибок
+// app.use((err, req, res, next) => {
+//     console.error('Unhandled error:', err);
+//     res.status(500).json({ error: 'Internal Server Error' });
+// });
 
 const PORT = 8020;
 
@@ -28,24 +28,41 @@ const PORT = 8020;
 // const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(async () => {
-        console.log('MongoDB connected');
-
-        const WEBHOOK_URL = `${process.env.BASE_URL}/bot/webhook/${bot.token}`; // Убедитесь, что BASE_URL настроен правильно
-        await bot.telegram.setWebhook(WEBHOOK_URL);
-        console.log(`Webhook установлен на ${WEBHOOK_URL}`);
-
-        app.use(bot.webhookCallback(`/bot/webhook/${bot.token}`));
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+// Подключение к MongoDB
+const start = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
-    })
-    .catch(err => {
-        console.error('MongoDB connection error:', err);
+        console.log('Successfully connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+        process.exit(1);
+    }
+
+    // Настройка Webhook для Telegram
+    const WEBHOOK_URL = `${process.env.BASE_URL}/bot/webhook/${bot.token}`; // Убедитесь, что BASE_URL настроен правильно
+    await bot.telegram.setWebhook(WEBHOOK_URL);
+    console.log(`Webhook установлен на ${WEBHOOK_URL}`);
+
+    // Используем Webhook-колбэк
+    app.use(bot.webhookCallback(`/bot/webhook/${bot.token}`));
+
+    app.use('/payment', paymentRoutes);
+    app.use('/webhook', webhookRoutes);
+
+    app.use((err, req, res, next) => {
+        console.error('Unhandled error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
     });
 
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+start();
 
 process.once('SIGINT', () => {
     bot.stop('SIGINT');
